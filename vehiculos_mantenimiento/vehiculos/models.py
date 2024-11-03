0c8ffa797acb618a61055c4ca1999a8fe2c1b7e3
Santiago Mesa <thiagomesa21@gmail.com>
2024-11-03 11:47:38 -0500

    fin de clase 2
    se agrega modelo Manetenimiento.

--- vehiculos_mantenimiento/vehiculos/models.py
+++ vehiculos_mantenimiento/vehiculos/models.py
@@ -10,3 +10,13 @@ class Vehiculo(models.Model):
     def __str__(self):
         return str(self.nombre)
 
+class Mantenimiento(models.Model):
+    vehiculo = models.ForeignKey('Vehiculo', on_delete=models.CASCADE)
+    fecha = models.DateField()
+    observaciones = models.TextField()
+    evidencia_fotografica = models.ImageField(upload_to='mantenimientos/fotos', blank=True, null=True)
+    factura = models.FileField(upload_to='mantenimientos/facturas', blank=True, null=True)
+
+    def __str__(self):
+        return f"Mantenimiento del {self.fecha} para {self.vehiculo.nombre}"
+
