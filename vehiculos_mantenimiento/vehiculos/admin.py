10639565abf9e5525ff49febe1035095812c50f8
Santiago Mesa <thiagomesa21@gmail.com>
2024-11-03 18:51:21 -0500

    Finalizacion clase 2
    Se personaliza la vista administrativa para mantenimientos.

--- vehiculos_mantenimiento/vehiculos/admin.py
+++ vehiculos_mantenimiento/vehiculos/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Vehiculo, Mantenimiento
 
+class MantenimientoAdmin(admin.ModelAdmin):
+    list_display = ('vehiculo', 'fecha', 'observaciones_breve')
+
+    def observaciones_breve(self , obj):
+        return obj.observaciones[:100]
+    observaciones_breve.short_description = 'Observaciones'
+
+
 # Register your models here.
 admin.site.register(Vehiculo)
-admin.site.register(Mantenimiento)
+admin.site.register(Mantenimiento, MantenimientoAdmin)
